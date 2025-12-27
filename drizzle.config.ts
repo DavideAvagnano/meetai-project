b62a3dbe661316abab9440bd2de7a3120924ebc6
DavideAvagnano <dav.avagnano@gmail.com>
2025-12-27 19:40:02 +0100

    auth setup

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
 
-const folderPath = "/.src/db";
+const folderPath = "./src/db";
 
 export default defineConfig({
   out: `${folderPath}/migrations`,
