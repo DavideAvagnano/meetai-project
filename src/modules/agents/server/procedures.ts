129b2e258c2798e512157f0c0cc5f2003beadf7f
DavideAvagnano <dav.avagnano@gmail.com>
2026-01-22 22:47:05 +0100

    implement new agents form

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -1,9 +1,34 @@
+import z from 'zod';
 import { db } from '@/db';
 import { agents } from '@/db/schema';
-import { baseProcedure, createTRPCRouter } from '@/trpc/init';
+import { createTRPCRouter, protectedProcedure } from '@/trpc/init';
+import { agentsInsertSchema } from '../schemas';
+import { and, count, desc, eq, getTableColumns, ilike } from 'drizzle-orm';
+import { TRPCError } from '@trpc/server';
+import { DEFAULT_PAGE, DEFAULT_PAGE_SIZE, MAX_PAGE_SIZE, MIN_PAGE_SIZE } from '@/constants';
 
 export const agentsRouter = createTRPCRouter({
-  getMany: baseProcedure.query(async () => {
-    return await db.select().from(agents);
+  create: protectedProcedure.input(agentsInsertSchema).mutation(async ({ input, ctx }) => {
+    const [createdAgent] = await db
+      .insert(agents)
+      .values({
+        ...input,
+        userId: ctx.auth.user.id,
+      })
+      .returning();
+
+    return createdAgent;
+  }),
+
+  getOne: protectedProcedure.input(z.object({ id: z.string() })).query(async ({ input, ctx }) => {
+    const [existingAgent] = await db.select().from(agents).where(eq(agents.id, input.id));
+
+    return existingAgent;
+  }),
+
+  getMany: protectedProcedure.query(async ({ ctx, input }) => {
+    const data = await db.select().from(agents);
+
+    return data;
   }),
 });
